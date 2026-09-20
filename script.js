3d6e9086acc0efb7628e89bf685a97224945408b
ecoclicks <thissfiless@gmail.com>
2026-09-20 13:37:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const INSTAGRAM_USERNAME = "ecow.click";
 const products = [
   {
     id: 1,
-    name: "Eco Click charm",
+    name: "Eco Click Charm",
     price: "",
     image: "prod1.JPG",
     description: "Pilih warna gelang dengan memilih angka. Setiap gelang punya warna dan cerita tersendiri. Manik Handmade pilihan. Ringan & nyaman di pakai. Tali adjustable (15-20 cm). Cocok untuk daily look, sekolah, atau kado."
